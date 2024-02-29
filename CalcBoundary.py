f71eee684551d00ffdba03476e9583bdc80ed3d3
KamijouMikoto12311 <2553032814@qq.com>
2024-02-29 23:07:51 +0800

    fixed filename bug

--- CalcBoundary.py
+++ CalcBoundary.py
@@ -172,6 +172,6 @@ for wdir in wdirs:
     numdir += 1
     images = []
     for i in range(1000):
-        filename = f"imgs/f{i+1}.png"
+        filename = f"imgs/{i+1}.png"
         images.append(imageio.imread(filename))
     imageio.mimsave("plot_animation.gif", images, fps=5)
